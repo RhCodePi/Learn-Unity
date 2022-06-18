a6acc35107ab33f39311e4b94aa8bad0a695c7f5
Abdullah <abdullahv2730@gmail.com>
2022-06-18 20:14:27 +0300

    Upload Prototype 1

---
+++ Prototype 1/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private GameObject player;
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 15f;
+    [SerializeField] private float turnSpeed = 45f;
+    private float horizontal;
+    private float vertical;
+
+    void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward  * Time.deltaTime * speed * vertical);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontal);
+    }
+}
