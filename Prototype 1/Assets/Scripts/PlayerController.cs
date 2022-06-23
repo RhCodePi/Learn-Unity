06dd6c454d5a3e21d606e74f4397167f972d3559
Abdullah <abdullahv2730@gmail.com>
2022-06-23 12:06:36 +0300

    update

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 15f;
+    [SerializeField] private float speed;
+    [SerializeField] float rpm;
+    [SerializeField] private float horseSpeed;
     [SerializeField] private float turnSpeed = 45f;
     private float horizontal;
     private float vertical;
+    private Rigidbody playerRb;
+    [SerializeField] GameObject centerOfMass;
+    [SerializeField] TextMeshProUGUI speedoMeter;
+    [SerializeField] TextMeshProUGUI rpmText;
+    [SerializeField] List<WheelCollider> allWheels;
+    private int wheelGround;
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
+    }
 
     void FixedUpdate()
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.forward  * Time.deltaTime * speed * vertical);
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontal);
+        //transform.Translate(Vector3.forward  * Time.deltaTime * speed * vertical);
+        if (OnIsGround())
+        {
+            playerRb.AddRelativeForce(Vector3.forward * horseSpeed * vertical);
+
+            transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontal);
+
+            speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 3.6f); // 3.6f for kpm
+            speedoMeter.SetText("Speed : " + speed + "Kmp");
+
+            rpm = Mathf.RoundToInt(speed % 30);
+            rpmText.SetText("RMP : " + rpm);
+
+        }    
     }
+
+    bool OnIsGround()
+    {
+        wheelGround = 0;
+        foreach(WheelCollider wheel in allWheels)
+        {
+            if (wheel.isGrounded) wheelGround++;
+        }
+
+        return wheelGround == 4 ? true : false;
+    }
+
 }
